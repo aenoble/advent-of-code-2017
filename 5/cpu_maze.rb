bda16975e0ca0951846238ed665889b398ba2e9e
Andrew Noble <aenoble@gmail.com>
2017-12-05 12:29:22 -0500

    Day 5a.

---
+++ 5/cpu_maze.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+
+class CPUMazeTest < Test::Unit::TestCase
+  TEST_INPUT = "test_input.txt"
+  def test_case_1
+    assert_equal(cpu_maze(TEST_INPUT), 5)
+  end
+end
+
+def cpu_maze(file_path)
+  maze = File.readlines(file_path).map(&:to_i)
+  index = 0
+  steps = 0
+  while index < maze.length
+    step_inc = maze[index]
+    maze[index] = step_inc + 1
+    index += step_inc
+    steps += 1
+  end
+  steps
+end
+
+puts cpu_maze "input.txt"
