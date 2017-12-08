6f366256ca93e3c2e1878ec0d7ad76afcc39c0ae
Andrew Noble <aenoble@gmail.com>
2017-12-08 17:24:32 -0500

    Day 6b.

---
+++ 6/memory_allocation_2.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+
+class MemoryAllocationTest < Test::Unit::TestCase
+  def test_case_1
+    assert_equal(memory_allocation([0, 2, 7, 0]), 4)
+  end
+end
+
+def memory_allocation(blocks)
+  patterns = []
+  while true
+    value = blocks.max
+    index = blocks.find_index(value)
+    blocks[index] = 0
+    step = 0
+    while step < value
+      step += 1 
+      next_index = (index + step) % (blocks.length)
+      blocks[next_index] += 1
+    end
+    patterns << blocks.dup
+    break if patterns.length > 1 && (patterns.length != patterns.uniq.length)
+  end
+  (patterns.length - 1) - patterns.find_index(patterns.last)
+end
